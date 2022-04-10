2533c2e7f560c25a178b72dbc984cffb1e95965a
MrSetOne <lara.sanchez.michael@gmail.com>
2022-04-10 18:47:45 +0200

    scroll button upgraded

--- js/scroll.js
+++ js/scroll.js
@@ -1,3 +1,6 @@
 const todown = () => {
-    document.getElementById("cuerpo").className = "scrolleable";
+    document.getElementById("cuerpo").style.overflowY = "scroll";
+    document.getElementById("all__hero").style.height = "30rem";
+    document.getElementById("text__hero").style.display = "none";
+    document.getElementById("btn__container").style.display = "none";
 }
