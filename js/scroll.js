f71a8772f40b247d833c6b00c7d8ae0ac9a77cc5
MrSetOne <lara.sanchez.michael@gmail.com>
2022-04-11 23:09:39 +0200

    index card mobile only

--- js/scroll.js
+++ js/scroll.js
@@ -4,5 +4,25 @@ const todown = (t) => {
         document.getElementById("all__hero").style.height = "0";
         document.getElementById("text__hero").style.display = "none";
         document.getElementById("btn__container").style.display = "none";
-    }, t)
+    }, t);
+};
+
+const showme__front = () => {
+    document.getElementById("card__frontend").style.top = "50%";
+    document.getElementById("card__frontend").style.opacity = "1";
+}
+
+const hideme__front = () => {
+    document.getElementById("card__frontend").style.top = "30%";
+    document.getElementById("card__frontend").style.opacity = "0";
+}
+
+const showme__back = () => {
+    document.getElementById("card__backend").style.top = "50%";
+    document.getElementById("card__backend").style.opacity = "1";
+}
+
+const hideme__back = () => {
+    document.getElementById("card__backend").style.top = "30%";
+    document.getElementById("card__backend").style.opacity = "0";
 }
